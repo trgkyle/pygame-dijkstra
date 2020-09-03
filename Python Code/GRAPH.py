2df4caec54f384df2d8a8835cb7ca634cdb1a0f4
aman0303 <39325082+aman0303@users.noreply.github.com>
2020-09-03 16:40:10 +0530

    Update GRAPH.py

--- Python Code/GRAPH.py
+++ Python Code/GRAPH.py
@@ -21,29 +21,30 @@ pygame.display.set_caption("Play with graphs!")
 left_background = pygame.image.load('background.png')
 node1 = pygame.image.load('r_circle.png')
 node2 = pygame.image.load('b_circle.png')
+node3 = pygame.image.load('y_circle.png')
 plus = pygame.image.load('plus.png')
 add = pygame.image.load('add.png')
 cross = pygame.image.load('cross.png')
 algo_button = pygame.image.load('algo_button.png')
 
-button_font = pygame.font.Font('MountainBridge.otf', 20)
+button_font = pygame.font.Font('roboto.ttf', 20)
 msg_font = pygame.font.Font('roboto.ttf', 15)
 
 add_node = button_font.render('Add Nodes', True, WHITE)
 add_edge = button_font.render('Add Edges', True, WHITE)
 dfs_button = button_font.render('DFS', True, WHITE) 
 bfs_button = button_font.render('BFS', True, WHITE)
-mst_button = button_font.render('Find MST', True, WHITE)
-djikstra_button = button_font.render('Djikstra', True, WHITE) 
+find_bridges_button = button_font.render('Find Bridges', True, WHITE)
+clear_button = button_font.render('Clear Screen', True, WHITE) 
 msg_box = msg_font.render('', True, BLUE);
 
 node_button = plus
 edge_button = add
 nodes = []
 edges = []
-weight = []
 yellow_edges = []
-color = [node2,node1]
+blue_edges = []
+color = [node2,node1,node3]
 node_color = []
 pos = (-1,-1)
 pointA = -1
@@ -117,15 +118,80 @@ def start_bfs(point):
     for i in range(len(edges)):
         adj[edges[i][0]].append(edges[i][1])
     bfs(point,dis,adj)
-    
-def getNode(mos_x,mos_y):
-    for i in range(len(nodes)):
-        x1 = nodes[i][0]
-        y1 = nodes[i][1]
-        if isClicked(x1, y1, x1 + node2.get_width(), y1 + node2.get_height(), mos_x, mos_y):
-            return i
-    return -1
 
+def find_bridges(u,counter,dfs_num,dfs_low,par,adj):
+    counter = counter + 1
+    dfs_num[u] = counter
+    dfs_low[u] = counter
+    ch_count = 0
+    
+    node_color[u] = color[1]
+    show_edges()
+    show_nodes()
+    pygame.display.update()
+    pygame.time.delay(200)
+    
+    for i in range(len(adj[u])):
+        v = adj[u][i]
+        if par[u] == v:
+            continue
+        if dfs_num[v] == 0:
+            
+            yellow_edges.append((u,v))
+            yellow_edges.append((v,u))
+            show_edges()
+            show_nodes()
+            pygame.display.update()
+            pygame.time.delay(200)
+            
+            ch_count = ch_count + 1
+            par[v] = u
+            find_bridges(v,counter,dfs_num,dfs_low,par,adj)
+            dfs_low[u] = min(dfs_low[u],dfs_low[v])
+            
+            show = False
+            if par[u]!=-1 and dfs_low[v]>=dfs_num[u]:
+                show = True
+                node_color[u] = color[2]
+                
+            if dfs_low[v]>dfs_num[u]:
+                show = True
+                blue_edges.append((u,v))
+                blue_edges.append((v,u))
+                
+            if show:
+                show_edges()
+                show_nodes()
+                pygame.display.update()
+                pygame.time.delay(500)
+        else:
+            dfs_low[u] = min(dfs_low[u],dfs_num[v])
+            
+    if ch_count>1 and par[u]==-1:
+        node_color[u] = color[2]
+        show_edges()
+        show_nodes()
+        pygame.display.update()
+        pygame.time.delay(200)
+
+def start_finding_bridges():
+    n = len(nodes)
+    m = len(edges)
+    if(n==0 or m==0):
+        return
+    adj = [[] for i in range(n)]
+    for i in range(m):
+        adj[edges[i][0]].append(edges[i][1])
+    counter = 0
+    dfs_num = [0 for i in range(n)]
+    dfs_low = [0 for i in range(m)]
+    par = [0 for i in range(n)]
+    for i in range(n):
+        if dfs_num[i] == 0:
+            par[i] = -1
+            find_bridges(i,counter,dfs_num,dfs_low,par,adj)
+            counter = 0
+    
 def make_equal(listA, listB):
     for i in range(len(listA)):
         listA[i] =listB[i]
@@ -155,6 +221,14 @@ def ishovering(x1,y1,x2,y2):
     else:
         return False
 
+def getNode(mos_x,mos_y):
+    for i in range(len(nodes)):
+        x1 = nodes[i][0]
+        y1 = nodes[i][1]
+        if isClicked(x1, y1, x1 + node2.get_width(), y1 + node2.get_height(), mos_x, mos_y):
+            return i
+    return -1
+
 def show_nodes():
     if(len(nodes)==0): return
     for i in range(len(nodes)):
@@ -166,17 +240,19 @@ def show_edges():
             pygame.draw.line(screen,BLACK,(nodes[edges[i][0]][0]+16,nodes[edges[i][0]][1]+16),(nodes[edges[i][1]][0]+16,nodes[edges[i][1]][1]+16),1)
     for i in range(len(yellow_edges)):
             pygame.draw.line(screen,YELLOW,(nodes[yellow_edges[i][0]][0]+16,nodes[yellow_edges[i][0]][1]+16),(nodes[yellow_edges[i][1]][0]+16,nodes[yellow_edges[i][1]][1]+16),1)
- 
+    for i in range(len(blue_edges)):
+            pygame.draw.line(screen,BLUE,(nodes[blue_edges[i][0]][0]+16,nodes[blue_edges[i][0]][1]+16),(nodes[blue_edges[i][1]][0]+16,nodes[blue_edges[i][1]][1]+16),2)
+
 def show_buttons():
     if(state == 'start'):
         screen.blit(algo_button,(7,550))
-        screen.blit(djikstra_button,(7+algo_button.get_width()/2-30,550+algo_button.get_height()/2-10))
+        screen.blit(clear_button,(7+algo_button.get_width()/2-53,550+algo_button.get_height()/2-13))
         screen.blit(algo_button,(7,498))
-        screen.blit(dfs_button,(7+algo_button.get_width()/2-20,498+algo_button.get_height()/2-10))
+        screen.blit(dfs_button,(7+algo_button.get_width()/2-20,498+algo_button.get_height()/2-13))
         screen.blit(algo_button,(7,446))
-        screen.blit(bfs_button,(7+algo_button.get_width()/2-20,446+algo_button.get_height()/2-10))
+        screen.blit(bfs_button,(7+algo_button.get_width()/2-20,446+algo_button.get_height()/2-13))
         screen.blit(algo_button,(7,394))
-        screen.blit(mst_button,(7+algo_button.get_width()/2-40,394+algo_button.get_height()/2-10))
+        screen.blit(find_bridges_button,(7+algo_button.get_width()/2-50,394+algo_button.get_height()/2-13))
         
 def show_msg():
     msg_box = msg_font.render(msg, True, BLUE);
@@ -188,7 +264,7 @@ while running:
     screen.fill(WHITE)
     screen.blit(left_background,(0,0))
     
-    if(state == 'start' or state == 'add_node'):
+    if(state == 'start' or state == 'add_node' or state == 'exit'):
         screen.blit(node_button,(5,5))
         
     if(state == 'start' or state == 'add_edge1' or state == 'add_edge2'):
@@ -196,6 +272,7 @@ while running:
         
     show_buttons()
     show_msg()
+    
     if state == 'start':
         node_button = plus
         edge_button = add
@@ -221,6 +298,12 @@ while running:
         yellow_edges.clear()
         state = 'start'  
         point = -1
+    
+    if state == 'find_bridges':
+        temp_node = [color[0] for i in range(len(node_color))]
+        make_equal(temp_node,node_color)
+        start_finding_bridges()
+        state = 'exit'
         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -250,6 +333,16 @@ while running:
                             state = 'choose start point for bfs'
                             msg = 'Choose source for the Breadth First Search.'
                         else: state = 'start'
+                    elif(isClicked(7,394,7+algo_button.get_width(),394+algo_button.get_height(),pos[0],pos[1])):
+                        if len(nodes) != 0:
+                            node_button = cross
+                            state = 'find_bridges'
+                            msg = 'Articution Points: Yellow nodes    Bridges: Blue edges'
+                        else: state = 'start'
+                    elif(isClicked(7,550,7+algo_button.get_width(),550+algo_button.get_height(),pos[0],pos[1])):
+                        nodes.clear()
+                        node_color.clear()
+                        edges.clear()
                 elif state == 'add_node':
                     if pos[0]>200 and pos[1]<550:
                         nodes.append((pos[0]-16,pos[1]-16))
@@ -286,6 +379,13 @@ while running:
                     point  = getNode(pos[0],pos[1])
                     if point != -1:
                         state = 'bfs'
+                        msg = ''
+                elif state == 'exit':
+                    if(isClicked(5,5,5+node_button.get_width(),5+node_button.get_height(),pos[0],pos[1])):
+                        make_equal(node_color,temp_node)
+                        yellow_edges.clear()
+                        blue_edges.clear()
+                        state = 'start'
                         msg = ''
             pos = (-1,-1)
             
